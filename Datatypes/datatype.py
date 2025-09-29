077b152b84203795854a9d61645a9d6afa163f44
Abhishek Chaudhary <abhishekchaudhary8856@gmail.com>
2025-09-29 21:04:01 +0530

    variables in python

---
+++ Datatypes/datatype.py
@@ -0,0 +1,28 @@
+##Integer Data Type Example
+age = 35
+type(age)
+
+##Floating Point Data Type Example
+height = 5.11
+print(height)
+type(height)
+
+##String Data Type Example
+name = "Chris"
+print(name)
+type(name)
+
+##Boolean Data Type Example
+is_true = True
+type(is_true)
+
+##Boolean Comparison Example
+A = 10
+B = 10
+A == B
+type(A == B)
+
+##Common Errors with Data Types
+result = "hello" + 5
+result = "hello" + str(5)
+print(result)
